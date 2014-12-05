81bc93f76999df57fc5da45cb5933fb331d0d8e5
jonschlinkert <dev@sellside.com>
2014-12-05 08:13:54 -0500

    0.2.0
     - breaking changes!
     - see readme for API documentation

--- index.js
+++ index.js
@@ -1,88 +1,130 @@
 /**
- * digits
- * Copyright (c) 2013 Jon Schlinkert
+ * digits <https://github.com/jonschlinkert/digits>
+ *
+ * Copyright (c) 2013-2015 Jon Schlinkert
  * Licensed under the MIT License (MIT)
  */
 
 'use strict';
 
+var pad = require('pad-left');
+var longest = require('longest');
+
 /**
- * Pad numbers with zeros.
- * Automatically pad the number of digits based on the length of the array,
- * or explicitly pass in the number of digits to use.
- *
- * @param  {Number} num  The number to pad.
- * @param  {Object} opts Options object with `digits` and `auto` properties.
- *    {
- *      auto: array.length // pass in the length of the array
- *      digits: 4          // or pass in the number of digits to use
- *    }
- * @return {Number}      The padded number with zeros prepended
- *
- * @examples:
- *  1      => 000001
- *  10     => 000010
- *  100    => 000100
- *  1000   => 001000
- *  10000  => 010000
- *  100000 => 100000
+ * Expose `digits`
  */
 
-exports.pad = function (num, opts) {
-  var digits = opts.digits || 3;
-  if(opts.auto && typeof opts.auto === 'number') {
-    digits = String(opts.auto).length;
-  }
-  var lenDiff = digits - String(num).length;
-  var padding = '';
-  if (lenDiff > 0) {
-    while (lenDiff--) {
-      padding += '0';
-    }
-  }
-  return padding + num;
+module.exports = digits;
+
+/**
+ * Left pad the given `value` with the specified `number` of zeros
+ * or alternate `character`.
+ *
+ * ```js
+ * digits('abc', 10);
+ * //=> '0000000000abc'
+ *
+ * digits('abc', 10, '~');
+ * //=> '~~~~~~~~~~abc'
+ * ```
+ *
+ * @param {String} `value`
+ * @param {String} `number`
+ * @return {String} `character`
+ * @api public
+ */
+
+function digits(val, num, ch) {
+  return pad(val, num - val.length, ch);
+}
+
+/**
+ * Pad left pad each value in the given `array` to align with the
+ * longest item/number in the array. Zeros are used by default, but
+ * a custom `character` can be passed as the second argument.
+ *
+ * **Example**
+ *
+ * ```js
+ * digits.each([1, 100, 1000])
+ * //=> ['0001', '0100', '1000']
+ *
+ * digits.each([a, bb, ccc], ' ')
+ * //=> ['  a', ' bb', 'ccc']
+ * ```
+ *
+ * @param {Number} `arr` The array of values to pad.
+ * @param {Number} `len` Amount to pad the number.
+ * @return {String} Padded number
+ * @api public
+ */
+
+digits.each = function(arr, ch) {
+  var len = longest(arr).length;
+
+  return arr.map(function(ele) {
+    return digits(ele, len, ch);
+  });
 };
 
 /**
  * Strip leading digits from a string
- * @param  {String} str The string from which to strip digits
- * @return {String}     The modified string
+ *
+ * ```js
+ * digits.stripleft('010foo.md');
+ * // => "foo.md"
+ * ```
+ *
+ * @param  {String} `str`
+ * @return {String}
  */
 
-exports.stripleft = function(str) {
+digits.stripleft = function(str) {
   return str.replace(/^\d+\-?/g, '');
 };
 
 /**
  * Strip trailing digits from a string
- * @param  {String} str The string from which to strip digits
- * @return {String}     The modified string
+ *
+ * ```js
+ * digits.stripright('bar010.md');
+ * // => "bar.md"
+ * ```
+ * @param  {String} `str`
+ * @return {String}
  */
 
-exports.stripright = function(str) {
+digits.stripright = function(str) {
   return str.replace(/\-?\d+$/g, '');
 };
 
 /**
  * Count digits on the left side of a string
- * @param  {String} str The string with digits to count
- * @return {String}     The modified string
- * @example
- *  "001-foo.md" => 3
+ *
+ * ```js
+ * digits.countleft('001-foo.md');
+ * //=> 3
+ * ```
+ *
+ * @param  {String} `str`
+ * @return {String}
  */
 
-exports.countleft = function(str) {
+digits.countleft = function(str) {
   return String(str.match(/^\d+/g)).length;
 };
 
 /**
  * Count digits on the right side of a string
- * @param  {String} str The string with digits to count
- * @return {String}     The modified string
- * @example
- *  "001-foo.md" => 3
+ *
+ * ```js
+ * digits.countleft('foo-001.md');
+ * //=> 3
+ * ```
+ * @param  {String} `str`
+ * @return {String}
  */
 
-exports.countright = function(str) {
+digits.countright = function(str) {
   return String(str.match(/\d+$/g)).length;
 };
